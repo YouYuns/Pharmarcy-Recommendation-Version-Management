78139945828b6be63a99997dd3345fb57202aad4
shyun <tjdgh0312@naver.com>
2024-12-26 12:44:41 +0900

    first commit

---
+++ src/test/java/com/recommend/shyun/ShyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommend.shyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
