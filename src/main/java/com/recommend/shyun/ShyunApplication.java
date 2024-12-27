685b19f7dfdf9a9189f9f1fbdb625fb5a16eeb4c
shyun <tjdgh0312@naver.com>
2024-12-27 09:59:56 +0900

    db설정

--- src/main/java/com/recommend/shyun/ShyunApplication.java
+++ src/main/java/com/recommend/shyun/ShyunApplication.java
@@ -2,6 +2,7 @@ package com.recommend.shyun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ShyunApplication {
